09da4b0bbe83df161215573c70f1487b329b5dc9
Karine Gardère <102601138+titiyasuhiro@users.noreply.github.com>
2022-11-29 13:09:09 +0100

    Create bdd.php

---
+++ config/bdd.php
@@ -0,0 +1,7 @@
+<?php
+$dsn = 'mysql:dbname=xbau4383_txikifilms;host=localhost';
+$user = 'xbau4383_txiki';
+$password = 'con15ExR1vsJzDc5';
+
+$bdd = new PDO($dsn, $user, $password);
+//var_dump($bdd);
