f8a41d8305c38ed31e0c0fbeb0166cffa617bb4d
Karine Gardère <102601138+titiyasuhiro@users.noreply.github.com>
2023-01-02 14:19:09 +0100

    Create documentaire.php

---
+++ view/site/documentaire.php
@@ -0,0 +1,80 @@
+            <!--début contenu-->
+            <div class="carousel d-flex-column">
+                <div class="carousel-layout text-center">
+                    <img id="carousel-layout" src="img/illustrations/oeil-fond-transparent-txikifilms.png" alt="oeil" width="500" height="300">
+                </div>
+                <div class="carousel-only mt-5" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="carousel-img" src="img/documentaires/child-2612923__340.jpg" alt="child" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/documentaires/fishermen-5994018__340.jpg" alt="fisherman" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/documentaires/gaza-7460575__340.jpg" alt="gaza" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/documentaires/grassland-7319829__340.webp" alt="grassland" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/documentaires/man-6527045__340.jpg" alt="man1" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/documentaires/man-7266712__340.jpg" alt="man2" width="200" height="200">
+                        </div>
+                    </div>
+                </div>
+
+                <!-- DEBUT GRID CARD -->
+                <div class="grid grid-flow d-flex-wrap ms-5 mb-5">
+                    <div class="grid-ligne1 d-flex justify-content-center mt-3 gap-3">
+                        <div class="grid-ligne1-img1">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/documentaires/child-2612923__340.jpg" alt="child" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="grid-ligne1-img2">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/documentaires/fishermen-5994018__340.jpg" alt="fishermen" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="grid-ligne1-img3">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/documentaires/gaza-7460575__340.jpg" alt="gaza" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="grid-ligne2 d-flex justify-content-center mt-3 gap-3">
+                        <div class="grid-ligne2-img1">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/documentaires/grassland-7319829__340.webp" alt="grassland" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="grid-ligne2-img2">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/documentaires/man-6527045__340.jpg" alt="man1" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="grid-ligne2-img3">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/documentaires/man-7266712__340.jpg" alt="man2" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- FIN GRID CARD -->
+
+            <!--fin contenu-->
