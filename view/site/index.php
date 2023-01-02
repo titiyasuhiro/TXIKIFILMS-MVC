99ba85083e78e3e8c0109c694695f535be500169
Karine Gardère <102601138+titiyasuhiro@users.noreply.github.com>
2023-01-02 14:19:16 +0100

    Create index.php

---
+++ view/site/index.php
@@ -0,0 +1,49 @@
+            <!--début contenu-->
+            <div class="carousel d-flex">
+                <div class="carousel-layout">
+                    <img id="carousel-layout" src="img/illustrations/oeil-fond-transparent-txikifilms.png" alt="oeil" width="500" height="300">
+                </div>
+                <div class="carousel-only mt-5" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="carousel-img" src="img/illustrations/beauty-2423417__340.jpg" alt="beauty" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/illustrations/brooklyn-bridge-6399829__340.webp" alt="brooklyn" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/illustrations/child-562297__340.webp" alt="child" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/illustrations/desolee.jpg" alt="desolee" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/illustrations/eery-1648250__340.webp" alt="eery" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/illustrations/film1-102681__340.webp" alt="film1" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/illustrations/grain-3026099__340.webp" alt="grain" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/illustrations/house-2616607__340.jpg" alt="house" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/illustrations/mary-pickford-1963149__340.jpg" alt="mary" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/illustrations/monument-valley-618363__340.jpg" alt="monument" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/illustrations/the-main-production-2387257__340.webp" alt="main-production" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/illustrations/universal-studios-1640516__340.jpg" alt="universal" width="200" height="200">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--fin contenu-->
+        </main>
+        <!--fin main-->
