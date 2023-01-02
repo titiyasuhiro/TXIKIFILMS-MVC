f62769c4b8c90e83ce547b7e99b0a3d2075d0842
Karine Gardère <102601138+titiyasuhiro@users.noreply.github.com>
2023-01-02 14:19:12 +0100

    Create fiction.php

---
+++ view/site/fiction.php
@@ -0,0 +1,119 @@
+            <!--début contenu-->
+            <div class="carousel d-flex-column">
+                <div class="carousel-layout text-center">
+                    <img id="carousel-layout" src="img/illustrations/oeil-fond-transparent-txikifilms.png" alt="oeil" width="500" height="300">
+                </div>
+                <div class="carousel-only mt-5" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="carousel-img" src="img/fictions/beauty-2423417__340.jpg" alt="beauty" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/fictions/child-562297__340.jpg" alt="child" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/fictions/director-237412__340.jpg" alt="director" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/fictions/eery-1648250__340.jpg" alt="eery" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/fictions/grain-3026099__340.webp" alt="grain" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/fictions/house-2616607__340.jpg" alt="house" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/fictions/killer-4773702__340.jpg" alt="killer" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/fictions/man-4134645__340.jpg" alt="man" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/fictions/mary-pickford-1963155__340.webp" alt="mary" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/fictions/monument-valley-618363__340.jpg" alt="monument" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/fictions/star-wars-1724901__340.jpg" alt="star-wars" width="200" height="200">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- DEBUT GRID CARD -->
+            <div class="grid grid-flow d-flex-wrap ms-5 mb-5">
+                <div class="grid-ligne1 d-flex justify-content-center mt-3 gap-3">
+                    <div class="grid-ligne1-img1">
+                        <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                            <a href="<?= LienHelper::getLien('SiteController', 'film', 1); ?>" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                <img class="rounded-t-lg" src="img/fictions/beauty-2423417__340.jpg" alt="beauty" width="300" height="200">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="grid-ligne1-img2">
+                        <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                            <a href="<?= LienHelper::getLien('SiteController', 'film', 2) ?>" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                <img class="rounded-t-lg" src="img/fictions/child-562297__340.jpg" alt="child" width="300" height="200">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="grid-ligne1-img3">
+                        <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                            <a href="<?= LienHelper::getLien('SiteController', 'film', 3) ?>" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                <img class="rounded-t-lg" src="img/fictions/director-237412__340.jpg" alt="director" width="300" height="200">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="grid-ligne2 d-flex justify-content-center mt-3 gap-3">
+                    <div class="grid-ligne2-img1">
+                        <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                            <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                <img class="rounded-t-lg" src="img/fictions/eery-1648250__340.jpg" alt="eery" width="300" height="200">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="grid-ligne2-img2">
+                        <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                            <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                <img class="rounded-t-lg" src="img/fictions/grain-3026099__340.webp" alt="grain" width="300" height="200">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="grid-ligne2-img3">
+                        <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                            <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                <img class="rounded-t-lg" src="img/fictions/house-2616607__340.jpg" alt="house" width="300" height="200">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="grid-ligne3 d-flex justify-content-center mt-3 gap-3">
+                    <div class="grid-ligne3-img1">
+                        <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                            <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                <img class="rounded-t-lg" src="img/fictions/killer-4773702__340.jpg" alt="killer" width="300" height="200">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="grid-ligne3-img2">
+                        <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                            <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                <img class="rounded-t-lg" src="img/fictions/man-4134645__340.jpg" alt="man" width="300" height="200">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="grid-ligne3-img3">
+                        <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                            <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                <img class="rounded-t-lg" src="img/fictions/mary-pickford-1963155__340.webp" alt="mary" width="300" height="200">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- FIN GRID CARD -->
+
+            <!--fin contenu-->
