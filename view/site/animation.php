6408f7320b0b4b5635caa6972344c54ea3c78dec
Karine Gardère <102601138+titiyasuhiro@users.noreply.github.com>
2023-01-02 14:18:43 +0100

    Create animation.php

---
+++ view/site/animation.php
@@ -0,0 +1,83 @@
+            <!--début contenu-->
+            <div class="carousel d-flex-column">
+                <div class="carousel-layout text-center">
+                    <img id="carousel-layout" src="img/illustrations/oeil-fond-transparent-txikifilms.png" alt="oeil" width="500" height="300">
+                </div>
+                <div class="carousel-only mt-5" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="carousel-img" src="img/animations/barbie-1841835__340.jpg" alt="barbie" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/animations/figure-2789098__340.png" alt="figure" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/animations/hare-1631435__340.jpg" alt="hare" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/animations/joker-1225027__340.jpg" alt="joker" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/animations/photographer-1702074__340.jpg" alt="photographer" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/animations/snow-3852960__340.jpg" alt="snow" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/animations/young-3815057__340.webp" alt="young" width="200" height="200">
+                        </div>
+                    </div>
+                </div>
+
+                <!-- DEBUT GRID CARD -->
+                <div class="grid grid-flow d-flex-wrap ms-5 mb-5">
+                    <div class="grid-ligne1 d-flex justify-content-center mt-3 gap-3">
+                        <div class="grid-ligne1-img1">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/animations/barbie-1841835__340.jpg" alt="barbie" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="grid-ligne1-img2">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/animations/figure-2789098__340.png" alt="figure" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="grid-ligne1-img3">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/animations/hare-1631435__340.jpg" alt="hare" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="grid-ligne2 d-flex justify-content-center mt-3 gap-3">
+                        <div class="grid-ligne2-img1">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/animations/joker-1225027__340.jpg" alt="joker" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="grid-ligne2-img2">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/animations/photographer-1702074__340.jpg" alt="photographer" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="grid-ligne2-img3">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/animations/snow-3852960__340.jpg" alt="snow" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- FIN GRID CARD -->
+
+            <!--fin contenu-->
