23faedfe7eb7ee2935ddc6b54f5f4c51b637b255
Karine Gardère <102601138+titiyasuhiro@users.noreply.github.com>
2023-01-02 14:18:48 +0100

    Create autre.php

---
+++ view/site/autre.php
@@ -0,0 +1,174 @@
+            <!--début contenu-->
+            <div class="carousel d-flex-column">
+                <div class="carousel-layout text-center">
+                    <img id="carousel-layout" src="img/illustrations/oeil-fond-transparent-txikifilms.png" alt="oeil" width="500" height="300">
+                </div>
+                <div class="carousel-only mt-5" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="carousel-img" src="img/clips/concert-2527495__340.jpg" alt="concert1" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/clips/concert-2566002__340.jpg" alt="concert2" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/clips/istockphoto-1090388858-170667a.jpg" alt="stock1" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/clips/istockphoto-1288582512-170667a.jpg" alt="stock2" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/clips/monkey-3507317__340.webp" alt="monkey" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/clips/rock-1560867__340.jpg" alt="rock" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/clips/toy-3890797__340.jpg" alt="toy" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/clips/young-3890799__340.webp" alt="young" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/pubs/advertisement-2098989__340.jpg" alt="advertissement" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/pubs/drink-1870139_960_720.jpg" alt="drink" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/pubs/hard-rock-cafe-236022_960_720.jpg" alt="hard-rock-cafe" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/pubs/moulin-rouge-392147_960_720.jpg" alt="moulin-rouge" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/pubs/rain-3599085__340.jpg" alt="rain" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/institutionnel/apples-1841132__340.jpg" alt="apples" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/institutionnel/archive-1850170__340.webp" alt="archive" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/institutionnel/building-4781384__340.webp" alt="building" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/institutionnel/chess-316657__340.webp" alt="chess" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/institutionnel/computer-2565478__340.webp" alt="computer" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/institutionnel/despaired-2261021__340.webp" alt="despaired" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/institutionnel/hallway-3341001__340.webp" alt="hallway" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/institutionnel/innovation-561388__340.webp" alt="innovation" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/institutionnel/investment-5241253__340.webp" alt="investment" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/institutionnel/istockphoto-1319634209-170667a.jpg" alt="stock3" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/institutionnel/office-desk-6952919__340.jpg" alt="office-desk" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/institutionnel/people-6695318__340.jpg" alt="people" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/institutionnel/south-station-4927286__340.jpg" alt="south-station" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/institutionnel/stairs-6133971__340.jpg" alt="stairs" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/institutionnel/suit-673697__340.webp" alt="suit" width="200" height="200">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="carousel-img" src="img/institutionnel/woman-4702060__340.webp" alt="woman" width="200" height="200">
+                        </div>
+                    </div>
+                </div>
+
+                <!-- DEBUT GRID CARD -->
+                <div class="grid grid-flow d-flex-wrap ms-5 mb-5">
+                    <h5 class="autre-title text-center">CLIPS</h5>
+                    <div class="grid-ligne1 d-flex justify-content-center mt-3 gap-3">
+                        <div class="grid-ligne1-img1">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/clips/concert-2566002__340.jpg" alt="concert2" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="grid-ligne1-img2">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/clips/rock-1560867__340.jpg" alt="rock" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="grid-ligne1-img3">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/clips/toy-3890797__340.jpg" alt="toy" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <h5 class="autre-title text-center mt-2">PUBLICITES</h5>
+                    <div class="grid-ligne2 d-flex justify-content-center mt-3 gap-3">
+                        <div class="grid-ligne2-img1">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/pubs/advertisement-2098989__340.jpg" alt="advertissement" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="grid-ligne2-img2">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/pubs/rain-3599085__340.jpg" alt="rain" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="grid-ligne2-img3">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/pubs/drink-1870139_960_720.jpg" alt="drink" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <h5 class="autre-title text-center mt-2">INSTITUTIONNEL</h5>
+                    <div class="grid-ligne3 d-flex justify-content-center mt-3 gap-3 mb-3">
+                        <div class="grid-ligne3-img1">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/institutionnel/south-station-4927286__340.jpg" alt="south-station" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="grid-ligne3-img2">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/institutionnel/stairs-6133971__340.jpg" alt="stairs" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="grid-ligne3-img3">
+                            <div class="rounded-lg shadow-lg bg-white max-w-xs">
+                                <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    <img class="rounded-t-lg" src="img/institutionnel/hallway-3341001__340.webp" alt="hallway" width="300" height="200">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- FIN GRID CARD -->
+
+                    <!--fin contenu-->
