d68ca70fdca7938067b4318083986835a7a12ab1
Karine Gardère <102601138+titiyasuhiro@users.noreply.github.com>
2023-01-02 14:18:41 +0100

    Create actualite.php

---
+++ view/site/actualite.php
@@ -0,0 +1,65 @@
+                    <h1 class="titre-articles text-center">ACTUALITES</h1>
+                    <div class="grid d-flex mt-4 ms-4 gap-3 mb-4">
+                        <div class=".g-col-6 g-col-md-4">
+                            <article id="post-8032" class="et_pb_post clearfix et_pb_has_overlay et_pb_blog_item_0_0 post-8032 post type-post status-publish format-standard has-post-thumbnail hentry category-non-classifiee">
+                                <div class="et_pb_image_container">
+                                    <a href="" class="entry-featured-image-url">
+                                        <img loading="lazy" src="" alt="" class="" sizes="" width="" height="">
+                                    </a>
+                                </div> 
+                                <h2 class="entry-title">
+                                    <a class="link-article" href="">Au Bord du Monde d&rsquo;Amélia Nanni, sélectionné au MIA / Film co-production Market &#038; Pitching Forum</a>
+                                </h2>
+                                <p class="post-meta">
+                                    <span class="published">10 Oct 2022</span>
+                                </p>
+                                <div class="post-content">
+                                    <div class="post-content-inner">
+                                        <p>Bonne nouvelle pour Au Bord du Monde ! Le film d'Amelia Nanni est sélectionné au Festival Mia en co-production Market &amp; Pitching Forum ! Petit rappel du synopsis : Axelle 9 ans, mal aimée, est envoyée passer l'été chez ses grands-parents qu'elle n'a pas vu depuis...</p>
+                                    </div>
+                                    <a href="" class="more-link">lire plus</a>
+                                </div>
+                            </article> 
+                        </div>
+                        <div class=".g-col-6 g-col-md-4">
+                            <article id="post-8013" class="et_pb_post clearfix et_pb_has_overlay et_pb_blog_item_0_1 post-8013 post type-post status-publish format-standard has-post-thumbnail hentry category-non-classifiee">
+                                <div class="et_pb_image_container">
+                                    <a href="" class="entry-featured-image-url">
+                                        <img loading="lazy" src="" alt="" class="" srcset="" width="" height="" />
+                                    </a>
+                                </div> 
+                                <h2 class="entry-title">
+                                    <a class="link-article" href="">Nouvelle sélection pour « Le tombeau de l&rsquo;amiante »</a>
+                                </h2>
+                                <p class="post-meta">
+                                    <span class="published">21 Sep 2022</span>
+                                </p>
+                                <div class="post-content">
+                                    <div class="post-content-inner">
+                                        <p>Le formidable documentaire « Le tombeau de l&rsquo;amiante » de Nina Toussaint et Marie-Anne Mengeot ajoute un nouveau festival à son tableau de chasse. Ce dernier vient d'être sélectionné au festival Ciudad del Este independant film festival. Le beau parcours de ce film...</p>
+                                    </div>
+                                    <a href="" class="more-link">lire plus</a>
+                                </div>
+                            </article> 
+                        </div>
+                        <div class=".g-col-6 g-col-md-4">
+                            <article id="post-8001" class="et_pb_post clearfix et_pb_has_overlay et_pb_blog_item_0_2 post-8001 post type-post status-publish format-standard has-post-thumbnail hentry category-non-classifiee">
+                                <div class="et_pb_image_container">
+                                    <a href="" class="entry-featured-image-url">
+                                        <img loading="" src="" alt="" class="" srcset="" width="" height="" />
+                                    </a>
+                                </div> 
+                                <h2 class="entry-title">
+                                    <a class="link-article" href="">« Cette nuit-là » nommé au Prix Europa</a></h2>
+                                <p class="post-meta">
+                                    <span class="published">8 Sep 2022</span>
+                                </p>
+                                <div class="post-content">
+                                    <div class="post-content-inner">
+                                        <p>Le film de Safia Kessas "Cette nuit-là" est nommé au Prix Europa. Il s'agit aussi bien d'une fierté pour la réalisatrice que pour Iota Production qui a co-produit ce film fort et puissant dont les thématiques restent toujours d'actualité malheureusement. Encore toutes...</p>
+                                    </div>
+                                    <a href="" class="more-link">lire plus</a>
+                                </div>
+                            </article> 
+                        </div>
+                    </div>
