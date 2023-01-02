20572ed1ffdb1b2742e99827f831f30d4eac27e4
Karine Gardère <102601138+titiyasuhiro@users.noreply.github.com>
2023-01-02 14:18:45 +0100

    Create apropos.php

---
+++ view/site/apropos.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <h1 class="text-center mt-4 fw-bold">A PROPOS DE NOUS</h1>
+    <p>
+        Créée en 2023, Txiki Films s'implante au Pays Basque. Elle s'ouvre à la co-production et développe des partenariats.
+    </p>
+</div>
