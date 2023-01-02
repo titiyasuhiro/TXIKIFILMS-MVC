5c2073e27882007b20f0172d23fe5ab734fcdabd
Karine Gardère <102601138+titiyasuhiro@users.noreply.github.com>
2023-01-02 14:17:34 +0100

    Create header.php

---
+++ view/admin/communes/header.php
@@ -0,0 +1,69 @@
+<!--début header-->
+        <header class="header-container d-flex-column">
+            <div class="social-container d-inline-flex gap-1">
+                <div class="social-container-icon">
+                    <a class="social-container-icon-facebook" href="" target="_blank" title="facebook" style="font-size: 25px;">
+                        <img class="logo-social" src="img/illustrations/Facebook-logo.png" alt="logo-facebook" width="32" height="25">
+                    </a>
+                </div>
+                <div class="social-container-icon">
+                    <a class="social-container-icon-instagram" href="" target="_blank" title="instagram"
+                        style="font-size: 25px;">
+                        <img class="logo-social" src="img/illustrations/Instagram_logo_2016.svg.webp" alt="logo-instagram" (width="20" height="20")>
+                    </a>
+                </div>
+                <div class="social-container-icon">
+                    <a class="social-container-icon-linkedin" href="" target="_blank" title="linkedin" style="font-size: 25px;">
+                        <img class="logo-social" src="img/illustrations/logo-linkedin-icon-1536.png" alt="logo-linkedin" width="20" height="20">
+                    </a>
+                </div>
+                <div class="social-container-icon">
+                    <a class="social-container-icon-imdb" href="" target="_blank" title="imdb" style="font-size: 25px;">
+                        <img class="logo-social" src="img/illustrations/imdb-logo2.png" alt="logo-imbd" width="20" height="20">
+                    </a>
+                </div>
+                <div class="social-container-icon">
+                    <a class="social-container-icon-vimeo" href="" target="_blank" title="vimeo" style="font-size: 25px;">
+                        <img class="logo-social" src="img/illustrations/vimeo-icon-blue-logo.png" alt="logo-vimeo" width="20" height="20">
+                    </a>
+                </div>
+                <div class="social-container-icon">
+                    <a class="social-container-icon-youtube" href="" target="_blank" title="youtube" style="font-size: 25px;">
+                        <img class="logo-social" src="img/illustrations/youtube-logo-png-xl.png" alt="logo-youtube" width="20" height="20">
+                    </a>
+                </div>
+                <div class="social-container-icon">
+                    <a class="social-container-icon-mail" href="" target="_blank" title="mail" style="font-size: 25px;">
+                        <svg class="logo-social" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                            <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div class="title-container d-flex justify-content-center">
+                <div class="title d-inline-flex mt-2 mx-2">
+                    <p class="title1">TXIKI</p>
+                </div>
+                <div class="title-logo d-inline-flex mt-1">
+                    <a class="title-logo-txikifilms" href="" target="_blank" title="logo-site" style="font-size: 25px;">
+                        <img class="logo" src="img/illustrations/LOGO TXIKI FILMS.jpg" alt="logo-site" width="30" height="30">
+                    </a>
+                </div>
+                <div class="title d-inline-flex mt-2 ms-2">
+                    <p class="title2">FILMS</p>
+                </div>
+            </div>
+            <div class="header-search-bar d-flex justify-content-end mx-1">
+                <form action="" method="">
+                    <fieldset>
+                        <div class="input-group">
+                            <input class="search" name="search" type="text">
+                            <div class="input-group-btn">
+                                <button class="btn btn-danger" type="submit">Recherche</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </header>
+        <!--fin header-->
